c56cc0363f0cbb945a0a72c21affc8dd84393686
lnikon <nreplay@list.ru>
2018-07-14 19:02:04 +0400

    created getMaximalChildNumber() method

--- include/tree.h
+++ include/tree.h
@@ -9,21 +9,19 @@ class KTree final {
   struct Node {
     std::vector<std::shared_ptr<Node>> m_childs{};
 
-    Node() {}
+    Node(size_t nodesPerLevel = 0) { m_childs.reserve(nodesPerLevel); }
 
     size_t getChildsNumber() { return m_childs.size(); }
   };
 
-  KTree(size_t nodesPerLevel = 0) : m_nodesPerLevel(nodesPerLevel) {
+  KTree(size_t nodesPerLevel = 0) {
     assert(nodesPerLevel >= 0);
 
-    m_root = std::make_shared<Node>();
-    m_root->m_childs.resize(m_nodesPerLevel);
+    m_root = std::make_shared<Node>(nodesPerLevel);
   }
 
-  const auto getRoot() const {
-    return m_root;
-  }
+  const auto getRoot() const { return m_root; }
+  size_t getMaximalNodesNumber() { return m_nodesPerLevel; }
 
  private:
   std::shared_ptr<Node> m_root{};
