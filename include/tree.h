14adc8f5fdeebde8b16ed3bba78d711e44a4c285
lnikon <nreplay@list.ru>
2018-07-18 23:00:06 +0400

    partialy implemented insertion function()

--- include/tree.h
+++ include/tree.h
@@ -7,18 +7,32 @@ template <class T>
 class KTree final {
  public:
   struct Node {
+    T m_value{};
     std::vector<std::shared_ptr<Node>> m_siblings{};
 
-    Node(size_t nodesPerLevel = 0) { m_siblings.reserve(nodesPerLevel); }
+    Node(T value = T(), size_t nodesPerLevel = 0) : m_value(value) {
+      m_siblings.reserve(nodesPerLevel);
+    }
 
-    size_t getChildsNumber() { return m_siblings.size(); }
-
+    auto getChildsNumber() const { return m_siblings.size(); }
+    auto getMaximalNodesNumber() const { return m_siblings.capacity(); };
+    auto isFull() const { return (m_siblings.size() == m_siblings.capacity()); }
+    auto isEmpty() const { return (m_siblings.size() == 0); }
   };
 
   KTree(size_t nodesPerLevel = 0) {
     assert(nodesPerLevel >= 0);
 
     m_root = std::make_shared<Node>(nodesPerLevel);
+  }
+
+  auto insert(const T& value) {
+    if (!m_root.isFull()) {
+      auto getIndex = m_root->getChildsNumber();
+      m_root->m_siblings[getIndex + 1] =
+          std::make_shared<Node>(value, m_nodesPerLevel);
+    } else {
+    }
   }
 
   const auto getRoot() const { return m_root; }
